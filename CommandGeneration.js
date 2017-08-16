3a8e77b0bb103bd86bbcef892fda2faaff5d8c09
Trace Carrasco <TCarrasco@rightpoint.com>
2017-08-16 00:43:30 -0500

    Added the generate change method

--- CommandGeneration.js
+++ CommandGeneration.js
@@ -1,21 +1,21 @@
 function CommandGeneration() { }
 
+// Generates all the GET commands
+// Args: List of all possible block types
+// Returns: List of all valid GET commands for the block tpes
 CommandGeneration.prototype.GenerateGetCommands = function(block_types) {
-    // Generates all the GET commands
-    // Args: List of all possible block types
-    // Returns: List of all valid GET commands for the block tpes
-    var commands = new Array();
+    var commands = [];
     block_types.forEach(function (btype) {
         commands.push('get a ' + btype + ' block');
     })
     return commands;
 }
 
+// Generates all the MOVE commands
+// Args: List of all block IDs that are presume to be in the workspace
+// Returns: List of all valid MOVE commands
 CommandGeneration.prototype.GenerateMoveCommands = function(block_ids) {
-    // Generates all the MOVE commands
-    // Args: List of all block IDs that are presume to be in the workspace
-    // Returns: List of all valid MOVE commands
-    var commands = new Array();
+    var commands = [];
 	var even = false;
     block_ids.forEach(function (blockid1) {
 		if(even)
@@ -29,4 +29,24 @@ CommandGeneration.prototype.GenerateMoveCommands = function(block_ids) {
 		even = !even;
     });
     return commands;
+}
+
+// Generates all CHANGE commands
+// Args: block_ids: List of all block IDs that are presumed to be in the workspace
+//		 value_sets: List of all value sets for fields
+// Returns: List of all CHANGE commands
+
+CommandGeneration.prototype.GenerateChangeCommands = function(block_ids, value_sets) {
+	var commands = [];
+	block_ids.forEach(function (bid) {
+		value_sets.forEach(function(value_set) {
+			for(var i = 0; i < value_set.length - 1; i++) {
+				for(var j = i + 1; j < value_set.length; j++) {
+					commands.push('change ' + value_set[i] + ' in block ' + bid + ' to ' + value_set[j]);
+					commands.push('change ' + value_set[j] + ' in block ' + bid + ' to ' + value_set[i]);
+				}
+			}
+		});
+	});
+	return commands;
 }
